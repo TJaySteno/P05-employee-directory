1b0391c208c1b55884545f29b9aed82bc657849d
Trent <trent.stenoien@gmail.com>
2018-03-25 19:12:05 -0500

    Employee divs styled, search input added

--- js/app.js
+++ js/app.js
@@ -1,24 +1,54 @@
-$('document').ready(() => {
-  $.ajax({
-    url: 'https://randomuser.me/api/?inc=name,location,email,picture&results=12',
-    dataType: 'json',
-    success: function(data) {
-      // Constructor function for creating employee divs
-      function $Employee (employee) {
-        return $(`
-          <fieldset class="employee">
-          <img class="picture" src="${employee.picture.large}">
-          <h3 class="name">${employee.name.first} ${employee.name.last}</h3>
-          <p class="email">${employee.email}</p>
-          <p class="city">${employee.location.city}</p>
-          </fieldset>
-        `);
-      }
+$.ajax({
+  url: 'https://randomuser.me/api/?results=12',
+  dataType: 'json',
+  success: function(data) {
+    // Constructor function for creating employee divs
+    function $Employee (employee, gridPosition) {
+      let firstName = employee.name.first;
+      firstName = firstName.replace(firstName[0], firstName[0].toUpperCase());
+      let lastName = employee.name.last;
+      lastName = lastName.replace(lastName[0], lastName[0].toUpperCase());
+      const fullName = `${firstName} ${lastName}`;
+      let city = employee.location.city;
+      city = city.replace(city[0], city[0].toUpperCase());
 
-      // Create a fieldset for each employee retrieved
-      const $employees = $('.employees');
-      $(data.results).each(function () {
-        $employees.append(new $Employee(this)) });
+      return $(`
+        <fieldset class="employee ${gridPosition}">
+        <img class="picture" src="${employee.picture.large}">
+        <div class="info">
+          <h3 class="name">${fullName}</h3>
+          <p class="email">${employee.email}</p>
+          <p class="city">${city}</p>
+        </div>
+        </fieldset>
+      `);
     }
-  });
+
+    // Create a fieldset for each employee retrieved
+    const $employees = $('.employees');
+    let row = 0;
+    let column = 0;
+    $(data.results).each(function (i) {
+      const gridPosition = (() => {
+        if (i % 3 === 0) {
+          row++;
+          column = 1; }
+        else column++;
+        return `row${row} column${column}`;
+      })();
+      $employees.append(new $Employee(this, gridPosition)) });
+  }
+});
+
+$('document').ready(() => {
+  function findName (e) {
+    const searchTerm = $(e.target).val().toLowerCase();
+    $('.employees').children().each(function () {
+      const name = $(this).find('.name').text().toLowerCase();
+      if (name.indexOf(searchTerm) !== -1) $(this).show();
+      else $(this).hide();
+    });
+  }
+
+  $('.search').keyup(e => findName(e));
 });
