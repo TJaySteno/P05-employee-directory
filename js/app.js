0260de541e15e9d892c2c7a686f064466e4674c1
Trent <trent.stenoien@gmail.com>
2018-03-26 01:39:52 -0500

    Finalized for testing and review

--- js/app.js
+++ js/app.js
@@ -1,153 +1,189 @@
 $('document').ready(() => {
+  /************************************************
+    CREATE EMPLOYEE DIVS
+  ************************************************/
   $.ajax({
     url: 'https://randomuser.me/api/?results=12&nat=us',
     dataType: 'json',
-    success: function(data) {
-      // Constructor function for creating employee divs
-      function $Employee (employee) {
-        function capitalize (text) {
-          const words = text.split(' ');
-          let string = '';
-          words.forEach(function (word, i) {
-            if (/[a-z]/.test(word.charAt(0))) string += word.replace(word.charAt(0), word.charAt(0).toUpperCase());
-            else string += word;
-            if (i !== words.length-1) string += ' ';
-          });
-          return string;
+    success: (data) => {
+
+      // Return a string with first letters of all words capitalized
+      const capitalize = text => {
+        const words = text.split(' ');
+        let string = '';
+        $(words).each(function (i) {
+          if (/[a-z]/.test(this.charAt(0))) string += this.replace(this.charAt(0), this.charAt(0).toUpperCase());
+          else string += this;
+
+          if (i !== words.length-1) string += ' ';
+        });
+        return string;
+      }
+
+      // Return a given state's postal abbreviation
+      const abbreviate = state => {
+        const states = {
+          AL: "Alabama",
+          AK: "Alaska",
+          AZ: "Arizona",
+          AR: "Arkansas",
+          CA: "California",
+          CO: "Colorado",
+          CT: "Connecticut",
+          DE: "Delaware",
+          FL: "Florida",
+          GA: "Georgia",
+          GU: "Guam",
+          HI: "Hawaii",
+          ID: "Idaho",
+          IL: "Illinois",
+          IN: "Indiana",
+          IA: "Iowa",
+          KS: "Kansas",
+          KY: "Kentucky",
+          LA: "Louisiana",
+          ME: "Maine",
+          MD: "Maryland",
+          MA: "Massachusetts",
+          MI: "Michigan",
+          MN: "Minnesota",
+          MS: "Mississippi",
+          MO: "Missouri",
+          MT: "Montana",
+          NE: "Nebraska",
+          NV: "Nevada",
+          NH: "New Hampshire",
+          NJ: "New Jersey",
+          NM: "New Mexico",
+          NY: "New York",
+          NC: "North Carolina",
+          ND: "North Dakota",
+          OH: "Ohio",
+          OK: "Oklahoma",
+          OR: "Oregon",
+          PA: "Pennsylvania",
+          RI: "Rhode Island",
+          SC: "South Carolina",
+          SD: "South Dakota",
+          TN: "Tennessee",
+          TX: "Texas",
+          UT: "Utah",
+          VT: "Vermont",
+          VA: "Virginia",
+          WA: "Washington",
+          WV: "West Virginia",
+          WI: "Wisconsin",
+          WY: "Wyoming"
         }
+        for (let abbrev in states) if (state === states[abbrev].toLowerCase()) return abbrev;
+      }
 
-        function abbreviate (state) {
-          const states = {
-            AL: "Alabama",
-            AK: "Alaska",
-            AZ: "Arizona",
-            AR: "Arkansas",
-            CA: "California",
-            CO: "Colorado",
-            CT: "Connecticut",
-            DE: "Delaware",
-            FL: "Florida",
-            GA: "Georgia",
-            GU: "Guam",
-            HI: "Hawaii",
-            ID: "Idaho",
-            IL: "Illinois",
-            IN: "Indiana",
-            IA: "Iowa",
-            KS: "Kansas",
-            KY: "Kentucky",
-            LA: "Louisiana",
-            ME: "Maine",
-            MD: "Maryland",
-            MA: "Massachusetts",
-            MI: "Michigan",
-            MN: "Minnesota",
-            MS: "Mississippi",
-            MO: "Missouri",
-            MT: "Montana",
-            NE: "Nebraska",
-            NV: "Nevada",
-            NH: "New Hampshire",
-            NJ: "New Jersey",
-            NM: "New Mexico",
-            NY: "New York",
-            NC: "North Carolina",
-            ND: "North Dakota",
-            OH: "Ohio",
-            OK: "Oklahoma",
-            OR: "Oregon",
-            PA: "Pennsylvania",
-            RI: "Rhode Island",
-            SC: "South Carolina",
-            SD: "South Dakota",
-            TN: "Tennessee",
-            TX: "Texas",
-            UT: "Utah",
-            VT: "Vermont",
-            VA: "Virginia",
-            WA: "Washington",
-            WV: "West Virginia",
-            WI: "Wisconsin",
-            WY: "Wyoming"
-          }
+      // Return date of birth in more readable form
+      const formatBday = bday => bday.replace(/\d\d(\d\d)-(\d\d)-(\d\d).*/, '$2/$3/$1');
 
-          for (let abbrev in states) if (state === states[abbrev].toLowerCase()) return abbrev;
+      // Return an object filled with all essential info on an employee
+      const getEmployeeInfo = employee => {
+        const employeeInfo = {
+          name: capitalize(`${employee.name.first} ${employee.name.last}`),
+          phone: employee.phone.replace(/-/, ' '),
+          bday: `Birthday: ${formatBday(employee.dob)}`,
+          city: `${capitalize(employee.location.city)}, ${abbreviate(employee.location.state)}`
         }
-
-
-        let fullName = capitalize(`${employee.name.first} ${employee.name.last}`);
-
-        let phone = employee.phone.replace(/-/, ' ');
 
         let street = capitalize(employee.location.street);
-        let city = capitalize(employee.location.city);
-        let state = abbreviate(employee.location.state);
         let zip = employee.location.postcode;
-        const address = `${street}, ${city}, ${state}  ${zip}`;
+        employeeInfo.address = `${street}, ${employeeInfo.city}  ${zip}`;
 
-        const formatBday = bday => bday.replace(/\d\d(\d\d)-(\d\d)-(\d\d).*/, '$2/$3/$1');
-        let bday = `Birthday: ${formatBday(employee.dob)}`;
+        return employeeInfo;
+      }
+
+      // Construct and return an employee fieldset
+      function $Employee (employee) {
+        const employeeInfo = getEmployeeInfo(employee);
 
         return $(`
           <fieldset class="employee">
             <img class="picture" src="${employee.picture.large}">
             <div class="info">
-              <h3 class="name">${fullName}</h3>
+              <h3 class="name">${employeeInfo.name}</h3>
+              <p class="username">${employee.login.username}</p>
               <p class="email">${employee.email}</p>
-              <p class="city">${city}, ${state}</p>
-              <p class="phone">${phone}</p>
-              <p class="street">${address}</p>
-              <p class="bday">${bday}</p>
+              <p class="city">${employeeInfo.city}</p>
+              <p class="phone">${employeeInfo.phone}</p>
+              <p class="address">${employeeInfo.address}</p>
+              <p class="bday">${employeeInfo.bday}</p>
             </div>
           </fieldset>
         `);
       }
 
-      // Create a fieldset for each employee retrieved
+      // Create and display a fieldset for each employee retrieved
       $(data.results).each(function () {
         $('.employees').append(new $Employee(this)) });
 
-      const modalWindow = e => {
-        const $employee = (() => {
-          if ($(e.target).hasClass('employee')) return $(e.target);
-          else return $(e.target).parents('.employee');
-        })();
 
-        const empl = {
+
+      /************************************************
+        MODAL WINDOW
+      ************************************************/
+
+      // Redirect clicks on children of '.employee' div
+      const findEmplDiv = target => {
+        if ($(target).hasClass('employee')) return $(target);
+        else return $(target).parents('.employee');
+      };
+
+      // Return an object filled with information stored in an employee's fieldset
+      const getEmplInfoFromDiv = $employee => {
+        return {
           imgSrc: $employee.find('.picture').prop('src'),
           name: $employee.find('.name').text(),
+          username: $employee.find('.username').text(),
           email: $employee.find('.email').text(),
-          city: $employee.find('.city').text(),
           phone: $employee.find('.phone').text(),
-          street: $employee.find('.street').text(),
+          address: $employee.find('.address').text(),
           bday: $employee.find('.bday').text()
         }
+      }
+
+      // Create a pop-out window with info on a given employee
+      const createModalWindow = e => {
+        const $employee = findEmplDiv(e.target);
+        const emplInfo = getEmplInfoFromDiv($employee);
 
         const $modalWindow = $(`
           <div class="modal-window">
             <span class="modal-close">X</span>
-            <img class="modal-image" src="${empl.imgSrc}">
-            <h3 class="modal-name">${empl.name}</h3>
-            <p class="modal-email">${empl.email}</p>
-            <p class="modal-city">${empl.city}</p>
+            <img class="modal-image" src="${emplInfo.imgSrc}">
+            <h3 class="modal-name">${emplInfo.name}</h3>
+            <p class="modal-username">${emplInfo.username}</p>
+            <p class="modal-email">${emplInfo.email}</p>
             <hr>
-            <p class="modal-number">${empl.phone}</p>
-            <p class="modal-address">${empl.street}</p>
-            <p class="modal-birthday">${empl.bday}</p>
+            <p class="modal-number">${emplInfo.phone}</p>
+            <p class="modal-address">${emplInfo.address}</p>
+            <p class="modal-birthday">${emplInfo.bday}</p>
           </div>
         `);
 
+        // Add a listener for removal, remove any existing windows, and display the new one
         $modalWindow.find('.modal-close').click(e => $('.modal-window').remove());
-
         $('.modal-window').remove();
         $('body').append($modalWindow);
       }
 
-      $('.employees').click(e => { if (!$(e.target).hasClass('employees')) modalWindow(e) });
+      // Any click within an employee div will create a window for that employee
+      $('.employees').click(e => { if (!$(e.target).hasClass('employees')) createModalWindow(e) });
     }
   });
 
-  function findName (e) {
+
+
+  /************************************************
+    SEARCH FEATURE
+  ************************************************/
+
+  // Compare search value against names; show a div if it matches, otherwise hide it
+  const findName = e => {
     const searchTerm = $(e.target).val().toLowerCase();
     $('.employees').children().each(function () {
       const name = $(this).find('.name').text().toLowerCase();
